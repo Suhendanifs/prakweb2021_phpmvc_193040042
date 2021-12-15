56faa2d6b4ccdfba9490f6e46132ab357b9967e1
Dani <danicavrep@gmail.com>
2021-12-16 00:47:51 +0700

    Menyelesaikan tugas video ke-5 View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,21 @@
 <?php
 
-class About {
-    public function index($nama = 'Suhendani', $pekerjaan = 'Mahasiswa'){
-        echo "Hallo Nama saya $nama, Saya adalah $pekerjaan";
+class About extends Controller{
+    public function index($nama = 'Suhendani', $pekerjaan = 'Mahasiswa', $umur = 20)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        $this->view('template/header', $data);
+        $this->view('about/index', $data);
+        $this->view('template/footer');
     }
 
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'My Page';
+        $this->view('template/header', $data);
+        $this->view('about/page');
+        $this->view('template/footer');
     }
 }
---
+++ app/view/about/index.php
@@ -0,0 +1,5 @@
+
+    <h1>About me</h1>
+    <p>Hallo, Nama saya <?= $data['nama'];?> umur saya <?= $data['umur'];?>, saya adalah <?= $data['pekerjaan'];?></p>
+</body>
+</html>
